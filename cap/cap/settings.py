ee9689a8305bba0d215b48e11d2aa34ce4b5b6a6
Inu-Oh <ethanol-mace-swan@duck.com>
2024-12-21 21:10:07 -0500

    moved all style rules to static css/main.css

--- cap/cap/settings.py
+++ cap/cap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-qyaa9nzg!r8s#c_)y2d08h1vtf89)ghc5l957@^pg@a+a1civ2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,7 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "home/static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home/static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
